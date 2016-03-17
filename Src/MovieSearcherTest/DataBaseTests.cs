34e3360196d8b7813e4d20b2b2fce8ba6cb72761
Elizaveta Karpova <karpova.elizaveta@mail.ru>
2016-03-17 11:26:54 +0300

    Added test project and first test entry

---
+++ Src/MovieSearcherTest/DataBaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Movie_Searcher;
+
+namespace MovieSearcherTest
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void DatabaseFileCreate()
+        {
+            File.Delete("database.db");
+            var db = new Database();
+            db.Load();
+            db.Dispose();
+            Assert.IsTrue(File.Exists("database.db"));
+            File.Delete("database.db");
+        }
+    }
+}
